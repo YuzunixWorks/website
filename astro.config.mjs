e2b918c74b11fceaf8da8723d88a07b48a172100
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-05-17 09:57:15 +0900

    fix: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
     adapter: cloudflare(),
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: [
+    './src/**/*.{astro,html,js,ts,jsx,tsx,md}',
+    './components/**/*.{astro,js,ts}',
+    './layouts/**/*.{astro,js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
